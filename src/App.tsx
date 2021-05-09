a3c26f987b67bd60233920913b6a162fd408aac4
Enrico Graziani <enrico.graziani@cazoo.co.uk>
2021-05-09 22:49:21 +0100

    Cleaned up App

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,19 @@ import "./App.css";
 import { MyNavBar } from "./containers/MyNavBar";
 import { UserContextProvider } from "./contexts/user/UserContext";
 
-function App() {
-  return (
-    <React.StrictMode>
-      <UserContextProvider>
-        <Router>
-          {/* TODO {!isAuthenticating && (*/}
-          <div className="App container py-3">
-            <ErrorBoundary>
-              <MyNavBar />
-              <Routes />
-            </ErrorBoundary>
-          </div>
-        </Router>
-      </UserContextProvider>
-    </React.StrictMode>
-  );
-}
+const App: React.FC = () => (
+  <React.StrictMode>
+    <UserContextProvider>
+      <Router>
+        <div className="App container py-3">
+          <ErrorBoundary>
+            <MyNavBar />
+            <Routes />
+          </ErrorBoundary>
+        </div>
+      </Router>
+    </UserContextProvider>
+  </React.StrictMode>
+);
 
 export default App;
