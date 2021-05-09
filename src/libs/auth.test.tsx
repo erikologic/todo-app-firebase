f15014c566830b68055a4c413a88a1b7177d643f
Enrico Graziani <enrico.graziani@cazoo.co.uk>
2021-05-09 20:21:38 +0100

    added linter and prettier

--- src/libs/auth.test.tsx
+++ src/libs/auth.test.tsx
@@ -1,74 +1,72 @@
-/**
- * @jest-environment jsdom
- */
-import {auth} from "./firebase";
-import {render, RenderResult, act, fireEvent} from "@testing-library/react";
+import { auth } from "./firebase";
+import { render, RenderResult, act, fireEvent } from "@testing-library/react";
 import React from "react";
-import {UserContextProvider, useUserContext} from "./authContext";
+import { UserContextProvider, useUserContext } from "./authContext";
 import firebase from "firebase";
 
 const UserContextConsumer = () => {
-    const {user} = useUserContext()
-    return user
-        ? (
-            <div data-testid="authenticated" onClick={user.logout}>
-                {user.email}
-            </div>
-        )
-        : (
-            <div data-testid="not-authenticated">
-            </div>
-        )
-}
+  const { user } = useUserContext();
+  return user ? (
+    <div data-testid="authenticated" onClick={user.logout}>
+      {user.email}
+    </div>
+  ) : (
+    <div data-testid="not-authenticated"></div>
+  );
+};
 
-describe('authLib', () => {
-    let res: RenderResult;
+describe("authLib", () => {
+  let res: RenderResult;
 
-    beforeEach(() => {
-        res = render(
-            <UserContextProvider>
-                <UserContextConsumer/>
-            </UserContextProvider>
-        )
-    })
+  beforeEach(() => {
+    res = render(
+      <UserContextProvider>
+        <UserContextConsumer />
+      </UserContextProvider>
+    );
+  });
 
-    describe('when a user is not logged in', () => {
-        beforeEach(async () => {
-            await act(async () => {
-                await auth.signOut();
-            })
-        })
+  describe("when a user is not logged in", () => {
+    beforeEach(async () => {
+      await act(async () => {
+        await auth.signOut();
+      });
+    });
 
-        test('can know if is not logged in', async () => {
-            expect(await res.findByTestId("not-authenticated")).toBeInTheDocument()
-        })
-    })
+    test("can know if is not logged in", async () => {
+      expect(await res.findByTestId("not-authenticated")).toBeInTheDocument();
+    });
+  });
 
-    describe('when a user is logged in', () => {
-        beforeEach(async () => {
-            await act(async () => {
-                await auth.signOut();
-                await auth.signInWithCredential(firebase.auth.GoogleAuthProvider.credential(
-                    `{"sub": "abc123", "email": "foo@example.com", "email_verified": true}`
-                ));
-            })
-        })
+  describe("when a user is logged in", () => {
+    beforeEach(async () => {
+      await act(async () => {
+        await auth.signOut();
+        await auth.signInWithCredential(
+          firebase.auth.GoogleAuthProvider.credential(
+            `{"sub": "abc123", "email": "foo@example.com", "email_verified": true}`
+          )
+        );
+      });
+    });
 
-        afterEach(async () => {
-            await act(async () => {
-                await auth.signOut();
-            })
-        })
+    afterEach(async () => {
+      await act(async () => {
+        await auth.signOut();
+      });
+    });
 
-        test('can know if is logged in', async () => {
-            expect(await res.findByTestId("authenticated")).toBeInTheDocument()
-            expect(await res.findByTestId("authenticated")).toHaveTextContent('foo@example.com')
-        })
+    test("can know if is logged in", async () => {
+      expect(await res.findByTestId("authenticated")).toBeInTheDocument();
+      expect(await res.findByTestId("authenticated")).toHaveTextContent(
+        "foo@example.com"
+      );
+    });
 
-        test('can log out', async () => {
-            const authenticated = await res.findByTestId("authenticated");
-            fireEvent.click(authenticated)
-            expect(await res.findByTestId("not-authenticated")).toBeInTheDocument()
-        })
-    })
-})
+    test("can log out", async () => {
+      const authenticated = await res.findByTestId("authenticated");
+      fireEvent.click(authenticated);
+      expect(await res.findByTestId("not-authenticated")).toBeInTheDocument();
+    });
+  });
+});
--- src/libs/authContext.tsx
+++ src/libs/authContext.tsx
@@ -1,59 +1,56 @@
-import React, {useContext, createContext, useState, useEffect} from "react";
-import {auth, signInOptions} from "./firebase";
+import React, { useContext, createContext, useState, useEffect } from "react";
+import { auth, signInOptions } from "./firebase";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
 interface ToDoUser {
-    email: string | null;
-    logout: () => void;
+  email: string | null;
+  logout: () => void;
 }
 
 interface IAuthContext {
-    user?: ToDoUser,
-    SignInElem: React.FC;
+  user?: ToDoUser;
+  SignInElem: React.FC;
 }
 
 // Configure FirebaseUI.
 const uiConfig = {
-    // Popup signin flow rather than redirect flow.
-    signInFlow: 'popup',
-    // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-    signInSuccessUrl: '/',
-    // We will display Google and Facebook as auth providers.
-    signInOptions,
+  // Popup signin flow rather than redirect flow.
+  signInFlow: "popup",
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: "/",
+  // We will display Google and Facebook as auth providers.
+  signInOptions,
 };
 
 const SignInElem: React.FC = () => (
-    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth}/>
-)
+  <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+);
 
-const defaultUserContext = { SignInElem }
+const defaultUserContext = { SignInElem };
 const UserContext = createContext<IAuthContext>(defaultUserContext);
 export const useUserContext = () => useContext(UserContext);
 
-export const UserContextProvider = ({children}: any) => {
-    const [context, setContext] = useState<IAuthContext>(defaultUserContext)
+export const UserContextProvider = ({ children }: any) => {
+  const [context, setContext] = useState<IAuthContext>(defaultUserContext);
 
-    useEffect(() => {
-        const unregisterAuthObserver = auth.onAuthStateChanged(user => {
-            if (user) {
-                setContext({
-                    ...defaultUserContext,
-                    user: {
-                        logout: () => {
-                            auth.signOut().then(() => setContext(defaultUserContext))
-                        },
-                        email: user.email
-                    }
-                })
-            }
+  useEffect(() => {
+    const unregisterAuthObserver = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setContext({
+          ...defaultUserContext,
+          user: {
+            logout: () => {
+              auth.signOut().then(() => setContext(defaultUserContext));
+            },
+            email: user.email,
+          },
         });
-        return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
-    }, [])
+      }
+    });
+    return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+  }, []);
 
-
-    return (
-        <UserContext.Provider value={context}>
-            {children}
-        </UserContext.Provider>
-    )
-}
+  return (
+    <UserContext.Provider value={context}>{children}</UserContext.Provider>
+  );
+};
